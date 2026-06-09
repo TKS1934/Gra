d4e7cf5fa4b9a97ebf451fcb571077be6848a7f5
Marcin Bartoszak <marcin.bartoszak@student.put.poznan.pl>
2026-06-09 15:57:08 +0200

    Dodano w miarę działający hak przyciągający do ściany oraz skrzynkę do gracza

---
+++ Gra-main/blok.h
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+//#include "byt.h"
+
+class blok: public byt
+{
+protected:
+    bool isPFP; //is possible to go trough for player
+    bool isPFB; //is possible to go trough for block
+public:
+    int fispfp(){
+        return isPFP;}
+
+    bool fispfb() {
+        return isPFB;
+    }
+
+    blok(const sf::Vector2f& size1,const sf::Vector2f& position1, const sf::Vector2f& velocity1, const sf::RenderWindow& window,float graw,int ID, const sf::Texture* tex=nullptr) :
+        byt(size1, position1, velocity1, window,graw,ID,tex){
+        isPFP=(ID==3);
+        isPFB=(ID==2);
+        isStatic=1;
+
+
+        if(ID==0){
+            this->setFillColor(sf::Color::Transparent); // Powietrze pozostaje niewidoczne
+            isPFP = 1;
+            isPFB = 1;
+        }
+
+    }
+
+};
+
+
+
